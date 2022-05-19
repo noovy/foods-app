81306149b11a9af06320f4eea7e9e73def9deda5
Michał Nowotnik <micnowotnik@gmail.com>
2022-05-19 09:24:19 +0200

    handlink http errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,12 +7,18 @@ import MealItem from "./MealItem/MealItem";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://xpert-academy-1579184148150-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error('Ups... Something went wrong!');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -29,8 +35,11 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setIsLoading(false);
     };
-
-    fetchMeals();
+    
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
 
   }, []);
 
@@ -40,6 +49,12 @@ const AvailableMeals = () => {
         <p>Loading...</p>
       </section>
     );
+  }
+
+  if (httpError) {
+    return (<section className={classes.mealsloading}>
+      <p>{httpError}</p>
+    </section>);
   }
 
   const mealsList = meals.map((meal) => (
